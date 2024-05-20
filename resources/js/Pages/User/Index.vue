273e55009d10ad4be127f1573699a036b3f1fae6
Erick <erick020109@gmail.com>
2024-05-20 16:19:14 -0600

    feat: misc changes

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { router } from '@inertiajs/vue3';
 import UserLayouts from './Layouts/UserLayouts.vue';
+
 defineProps({
   products: Array
 })
@@ -12,7 +13,7 @@ const addToCart = (product) => {
       if (page.props.flash.success) {
         Swal.fire({
           toast: true,
-          icon: "sucess",
+          icon: "success",
           position: "top-end",
           showConfirmButton: false,
           title: page.props.flash.success
@@ -21,26 +22,35 @@ const addToCart = (product) => {
     }
   })
 }
-
 </script>
+
 <template>
   <UserLayouts>
     <div class="bg-white">
       <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 class="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
-
         <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           <div v-for="product in products" :key="product.id" class="group relative">
             <div
               class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/330px-No-Image-Placeholder.svg.png" :alt="product.imageAlt"
-                class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
-              <div @click="addToCart(product)">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-              </svg>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/330px-No-Image-Placeholder.svg.png"
+                :alt="product.imageAlt" class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+              <!-- add to cart icon -->
+              <div
+                class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 cursor-pointer ">
+
+                <div class="bg-blue-700 p-2 rounded-full">
+                  <a @click="addToCart(product)">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                      stroke="currentColor" class="w-6 h-6 text-white">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                    </svg>
+                  </a>
+
+
+                </div>
               </div>
             </div>
             <div class="mt-4 flex justify-between">
@@ -49,10 +59,9 @@ const addToCart = (product) => {
                   <a :href="product.href">
                     <span aria-hidden="true" class="absolute inset-0" />
                     {{ product.name }}
-                    {{ product.productType }}
                   </a>
                 </h3>
-                <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p>
+                <!-- <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p> -->
               </div>
               <p class="text-sm font-medium text-gray-900">{{ product.price }}</p>
             </div>
@@ -60,6 +69,5 @@ const addToCart = (product) => {
         </div>
       </div>
     </div>
-
   </UserLayouts>
 </template>
